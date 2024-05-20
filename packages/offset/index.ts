b9fed3e705642ca21b3fb69daded9128e3cd48f1
zbf <zbfcqtl@gmail.com>
2024-05-20 10:48:31 +0800

    fix: root element not considered

--- packages/offset/index.ts
+++ packages/offset/index.ts
@@ -1,9 +1,9 @@
-export const getRootOffset = (ele: HTMLElement, rootEle = document.documentElement) => {
+export const getRootOffset = (ele: HTMLElement, rootEle = document.body) => {
   let par = ele.offsetParent as HTMLElement;
   let offsetTop = ele.offsetTop;
   let offsetLeft = ele.offsetLeft;
 
-  while (par !== rootEle) {
+  while (par !== null && par !== rootEle) {
     offsetTop += par.offsetTop;
     offsetLeft += par.offsetLeft;
     par = par.offsetParent as HTMLElement;
